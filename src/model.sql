9d65d9d89148b61b22586a5f4c54627c33d2ab8d
Adam Gahverdiev <adam_gahverdiev@mail.ru>
2023-06-04 17:27:35 +0300

    refactoring table

--- src/model.sql
+++ src/model.sql
@@ -14,24 +14,24 @@ VALUES ('A'),
 CREATE TABLE city_way
 (
     id        SERIAL PRIMARY KEY,
-    from_city INTEGER REFERENCES city not null,
-    to_city   INTEGER REFERENCES city not null,
+    from_city varchar not null,
+    to_city   varchar not null,
     way       int                     not null
 );
 
 INSERT INTO city_way(from_city, to_city, way)
-VALUES (1, 2, 10),
-       (1, 3, 15),
-       (1, 4, 20),
-       (2, 1, 10),
-       (2, 3, 35),
-       (2, 4, 25),
-       (3, 1, 15),
-       (3, 2, 35),
-       (3, 4, 30),
-       (4, 1, 20),
-       (4, 2, 25),
-       (4, 3, 30);
+VALUES ('A', 'B', 10),
+       ('A', 'C', 15),
+       ('A', 'D', 20),
+       ('B', 'A', 10),
+       ('B', 'C', 35),
+       ('B', 'D', 25),
+       ('C', 'A', 15),
+       ('C', 'B', 35),
+       ('C', 'D', 30),
+       ('D', 'A', 20),
+       ('D', 'B', 25),
+       ('D', 'C', 30);
 
 SELECT *
 From city
